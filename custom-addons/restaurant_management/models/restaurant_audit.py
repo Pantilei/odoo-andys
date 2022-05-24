ea55a54e556b6666395c92b41aad94aae7fbc547
pantilei ianulov <ianulov.pantilei@gmail.com>
2022-05-24 23:58:14 +0300

    Add objet type action

--- custom-addons/restaurant_management/models/restaurant_audit.py
+++ custom-addons/restaurant_management/models/restaurant_audit.py
@@ -58,6 +58,9 @@ class RestaurantAudit(models.Model):
             },
         }
 
+    def save_and_create_new(self):
+        return self.env.ref("restaurant_management.restaurant_audit_inline_form_action").read()[0]
+
     # def _compute_fault_registry_json(self):
     #     pass
 
