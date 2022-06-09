6ce07241bc068063f89e148804dbc25f4f927fc4
pantilei ianulov <ianulov.pantilei@gmail.com>
2022-06-09 17:15:20 +0300

    Add sequence control to check list category

--- custom-addons/restaurant_management/models/check_list_category.py
+++ custom-addons/restaurant_management/models/check_list_category.py
@@ -6,8 +6,13 @@ from odoo import models, fields, api
 class CheckListCategory(models.Model):
     _name = 'restaurant_management.check_list_category'
     _description = 'Check List Category'
+    _order = "sequence asc"
 
     name = fields.Char(required=True)
+
+    sequence = fields.Integer(
+        string="Sequence"
+    )
 
     no_fault_category = fields.Boolean(
         string="No Fault Category",
