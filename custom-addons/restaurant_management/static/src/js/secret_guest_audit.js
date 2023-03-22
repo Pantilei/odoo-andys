d132ece272d264f5c088aa9843d9267c92c71afd
pantilei ianulov <ianulov.pantilei@gmail.com>
2023-03-23 00:18:18 +0200

    [FEAT]: Add event handlers for form validation

--- custom-addons/restaurant_management/static/src/js/secret_guest_audit.js
+++ custom-addons/restaurant_management/static/src/js/secret_guest_audit.js
@@ -10,11 +10,13 @@ odoo.define("restaurant_management.secret_guest_audit", function (require) {
 
   var _t = core._t;
 
-  
   publicWidget.registry.SecretGuestAudit = publicWidget.Widget.extend({
-    selector: ".o_form_main",
+    selector: ".o_forms_main",
     events: {
-      // "click .o_submit_button": "_onSubmit",
+      "click .o_submit_button": "_onSubmit",
+      "change .js_check_list": "_onCheckListCheck",
+      "change .js_check_list_with_note": "_onCheckListCheckWithNote",
+      // "submit .user_form,.check_list_form": "_onSubmitValidate"
     },
     custom_events: {},
 
@@ -31,7 +33,6 @@ odoo.define("restaurant_management.secret_guest_audit", function (require) {
         // self.$form = self.$("#review_form");
         // const queryParams = self._getQueryParams();
         // const lang = queryParams.lang;
-
         // self.$form.validate({
         //   // Specify validation rules
         //   rules: {
@@ -66,29 +67,47 @@ odoo.define("restaurant_management.secret_guest_audit", function (require) {
     // Handlers
     // -------------------------------------------------------------------------
 
-    _onSubmit: function (event) {
-      event.preventDefault();
-      const queryParams = this._getQueryParams();
-      const lang = queryParams.lang;
-      if (this.$form.valid()) {
-        var formData = new FormData(this.$form[0]);
-        let dataToSend = {};
-        formData.forEach(function (value, key) {
-          dataToSend[key] = value;
-        });
-
-        return this._rpc({
-          route: window.location.pathname + "/handle",
-          params: dataToSend,
-        }).then((r) => {
-          if (r.success) {
-            window.location.href =
-              window.location.pathname + `/thank-you?lang=${lang}`;
-          } else {
-            console.log(r.message);
-          }
-        });
+    _onCheckListCheck: function (event) {
+      if (event.target.value === "no") {
+        $(event.target).parent().parent().next().removeClass("d-none");
+      } else {
+        $(event.target).parent().parent().next().addClass("d-none");
       }
+    },
+
+    _onCheckListCheckWithNote: function (event) {
+      console.log(event.target.value);
+    },
+
+    _onSubmit: function (event) {
+      let forms = this.$el.find(".form_with_validation");
+      [...forms].forEach((form) => {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          // $(this).submit();
+        }
+        $(form).addClass("was-validated");
+      });
+      // if (this.$form.valid()) {
+      //   var formData = new FormData(this.$form[0]);
+      //   let dataToSend = {};
+      //   formData.forEach(function (value, key) {
+      //     dataToSend[key] = value;
+      //   });
+
+      //   return this._rpc({
+      //     route: window.location.pathname + "/handle",
+      //     params: dataToSend,
+      //   }).then((r) => {
+      //     if (r.success) {
+      //       window.location.href =
+      //         window.location.pathname + `/thank-you?lang=${lang}`;
+      //     } else {
+      //       console.log(r.message);
+      //     }
+      //   });
+      // }
     },
   });
 
