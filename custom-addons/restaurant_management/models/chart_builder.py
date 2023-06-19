6ba47117e29eae8fb226846bbaba8b11f6632a4b
pantilei ianulov <ianulov.pantilei@gmail.com>
2023-06-20 00:06:35 +0300

    [FEAT]: Add grouped horizontal chart in builder

--- custom-addons/restaurant_management/models/chart_builder.py
+++ custom-addons/restaurant_management/models/chart_builder.py
@@ -62,6 +62,71 @@ class ChartBuilder:
         
         return fig.to_html(config=config)
     
+    def build_grouped_horizontal_bar_chart(self, y, x1, x2):
+        layout = go.Layout(
+            # title='Double Line Chart',
+            xaxis=dict(
+                tickfont=dict(color='white'), 
+                titlefont=dict(color='white'), 
+                showgrid=True,
+                domain=[0.5, 1],
+            ),
+            yaxis=dict( 
+                showticklabels=False,
+                domain=[0, 1],
+                titlefont=dict(color='white'),
+                autorange="reversed"
+            ),
+            autosize=True,
+            height=self._height,
+            plot_bgcolor=self._paper_bgcolor,
+            paper_bgcolor=self._paper_bgcolor,
+            margin=dict(l=100, r=1, b=1, t=1, pad=1),
+            annotations=[
+                dict(
+                    xref='paper', 
+                    yref='y',
+                    x=0.5, 
+                    y=yd,
+                    xanchor='right',
+                    text=str(yd),
+                    font=dict(family='Arial', size=12, color='white'),
+                    showarrow=False, 
+                    align='right'
+                ) for yd in y
+            ],
+            barmode="group"
+        )
+
+        # Create the bar chart using plotly
+        fig = go.Figure()
+
+        fig.add_trace(go.Bar(
+            x=x1,  # Use values as the x-axis values
+            y=y,  # Use categories as the y-axis values
+            yperiodalignment="middle",
+            orientation='h',  # Set the orientation to horizontal
+            marker={"color": ["#71EDF1" for _ in y]},
+            # texttemplate='%{y}',  # Use %{y} to display the category names as labels
+            # textposition='auto',  # Set the position of the labels
+        ))
+
+        fig.add_trace(go.Bar(
+            x=x2,  # Use values as the x-axis values
+            y=y,  # Use categories as the y-axis values
+            yperiodalignment="middle",
+            orientation='h',  # Set the orientation to horizontal
+            marker={"color": ["#71EDF1" for _ in y]},
+            # texttemplate='%{y}',  # Use %{y} to display the category names as labels
+            # textposition='auto',  # Set the position of the labels
+        ))
+
+        fig.update_layout(layout)
+
+        config = {'displayModeBar': False}
+        
+        return fig.to_html(config=config)
+    
     def build_year_to_year_line_chart(self, x, y1, y2, label1, label2, upper_limit):
         # Create the first line chart trace
         trace1 = go.Scatter(
